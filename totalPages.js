9fb85c381f6c5d08d3b054ea933bc26aa22be78f
ChaySittichok <sittichok.3345@mail.kmutt.ac.th>
2022-09-10 20:44:05 +0700

    first

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,16 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if(arrayItems == null || undefined ){
+    return undefined
+  }
+  else if((rowsPerPage == null || undefined)||(rowsPerPage == 0)){
+    return 1
+  }
+  else{
+    let total = arrayItems.length/rowsPerPage
+    return Math.ceil(total)
+  }
+  
 }
 module.exports = totalPages
